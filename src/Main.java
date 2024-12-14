284325ca37cddbb991b0ae256f73b9d8ed17094b
id-214193831 <agam.hershkodek@mail.huji.ac.il>
2024-12-14 19:16:36 +0200

    add Main for testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import image.Image;
+import image.ImagePadding;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            Image image = new Image("cat.jpeg");
+                Image paddedImage = ImagePadding.padImage(image);
+            paddedImage.saveImage("newImg");
+        } catch (java.io.IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
