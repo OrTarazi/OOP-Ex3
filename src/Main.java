68476a6014c5b1945a495a9529e437ae04d5f0c0
id-214193831 <agam.hershkodek@mail.huji.ac.il>
2024-12-14 21:13:41 +0200

    add Main for testing

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import image.Image;
+import image.ImageDivision;
 import image.ImagePadding;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Image image = new Image("cat.jpeg");
+            Image image = new Image("board.jpeg");
                 Image paddedImage = ImagePadding.padImage(image);
             paddedImage.saveImage("newImg");
+            Image[][] subImages = ImageDivision.divideToImages(image, 2);
+            for (int i = 0; i < subImages.length; i++) {
+                for (int j = 0; j < subImages[i].length; j++) {
+                    subImages[i][j].saveImage("subImg" + i + "_" + j);
+                }
+            }
         } catch (java.io.IOException e) {
             System.out.println(e.getMessage());
         }
