5c793f1e77326b6c3f5305e07a22c7956e1cf10b
id-214193831 <agam.hershkodek@mail.huji.ac.il>
2024-12-15 21:20:28 +0200

    commit after second meeting- shell

---
+++ src/ascii_art/Shell.java
@@ -0,0 +1,15 @@
+package ascii_art;
+
+public class Shell {
+    public Shell() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void run(String imageName) {
+
+    }
+}
