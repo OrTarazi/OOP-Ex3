38ba9f39bdde48c9c474e9ae1e3cdfe80d697850
id-214193831 <agam.hershkodek@mail.huji.ac.il>
2024-12-19 11:41:24 +0200

    add documentation

--- src/image_char_matching/CharBrightnessCalculator.java
+++ src/image_char_matching/CharBrightnessCalculator.java
@@ -1,11 +1,25 @@
 package image_char_matching;
 
 /**
- * simply gets a boolean table and calculates the brightness level describing the character.
+ * Utility class for calculating the brightness level of a character.
+ *
+ * <p>The brightness is determined based on a boolean table representation of the character,
+ * where each `true` value represents a white pixel. The brightness is calculated as the ratio
+ * of white pixels to the total number of pixels.</p>
  *
  * @author Or Tarazi, Agam Hershko
  */
 public class CharBrightnessCalculator {
+    /**
+     * Calculates the brightness level of a given character.
+     *
+     * <p>The method converts the character into a boolean table representation,
+     * where `true` values represent white pixels and `false` values represent black pixels.
+     * The brightness is computed as the ratio of white pixels to the total number of pixels.</p>
+     *
+     * @param c the character for which brightness is to be calculated.
+     * @return the brightness level as a float between 0 (fully black) and 1 (fully white).
+     */
     public static float calculateCharBrightness(char c) {
         boolean[][] charBinaryImage = CharConverter.convertToBoolArray(c);
         int totalPixels = charBinaryImage.length * charBinaryImage[0].length;
