2e28470e9427d3b4c8440a4be5fba0cc241176c6
id-214193831 <agam.hershkodek@mail.huji.ac.il>
2024-12-19 09:46:08 +0200

    fix code style

--- src/ascii_art/BrightnessMemento.java
+++ src/ascii_art/BrightnessMemento.java
@@ -1,25 +1,34 @@
 package ascii_art;
 
+// todo: fix doc
 class BrightnessMemento {
     private boolean lastStateValid = true;
     private float[][] brightnessHistory;
 
-    public BrightnessMemento(float [][] subImageBrightnessMatrix) {
+    // todo: fix doc
+    public BrightnessMemento(float[][] subImageBrightnessMatrix) {
         this.saveState(subImageBrightnessMatrix);
     }
 
-    public boolean isLastStateValid(){return lastStateValid;}
+    // todo: fix doc
+    public boolean isLastStateValid() {
+        return this.lastStateValid;
+    }
 
-    public void setLastStateValidity(boolean validity){lastStateValid = validity;}
+    // todo: fix doc
+    public void setLastStateValidity(boolean validity) {
+        this.lastStateValid = validity;
+    }
 
+    // todo: fix doc
     // Save the current state of the originator
-    public void saveState(float [][] subImageBrightnessMatrix) {
+    public void saveState(float[][] subImageBrightnessMatrix) {
         this.brightnessHistory = subImageBrightnessMatrix;
     }
 
+    // todo: fix doc
     // Restore the last saved state of the originator
     public float[][] restoreState() {
         return this.brightnessHistory;
     }
-
 }
